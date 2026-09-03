e4a0a9fed9d4d8e4805e608f6e73ac581891d0c3
Naveen Siva <naveentanvi4806@gmail.com>
2026-09-03 07:04:35 +0530

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey:'AIzaSyCxNdOs69QAD-1FTIDVyf0N0N2YGqWNgOo',
+  authDomain:'nexcart-db-7a02f.firebaseapp.com',
+  projectId:'nexcart-db-7a02f',
+  storageBucket:'nexcart-db-7a02f.firebasestorage.app',
+  messagingSenderId:'868851396316',
+  appId:'1:868851396316:web:951e1988f3f2e4b065ce19'
+});
+
+firebase.messaging().onBackgroundMessage(function(payload){
+  var n=payload.notification||{},data=payload.data||{};
+  return self.registration.showNotification(n.title||data.title||'ZORRA',{body:n.body||data.body||'',icon:n.icon||'./icons/icon-192.png',badge:'./icons/icon-192.png',tag:data.tag||'zorra-order-update',data:{url:data.url||'./home.html'}});
+});
+self.addEventListener('notificationclick',function(event){event.notification.close();var url=(event.notification.data&&event.notification.data.url)||'./home.html';event.waitUntil(clients.matchAll({type:'window',includeUncontrolled:true}).then(function(list){for(var i=0;i<list.length;i++){if('focus' in list[i]){list[i].navigate(url);return list[i].focus();}}return clients.openWindow?clients.openWindow(url):null;}));});
